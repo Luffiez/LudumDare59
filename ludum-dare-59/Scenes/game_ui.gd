2a6ae3d0ff4df19d0c6a874d2f0579d9955a0a4e
Erik Rodriguez <Luffiez123@gmail.com>
2026-04-19 12:47:20 +0200

    fixed buttons

--- ludum-dare-59/Scenes/game_ui.gd
+++ ludum-dare-59/Scenes/game_ui.gd
@@ -20,7 +20,8 @@ func gain_score(score_to_add:int)->void:
 func on_game_over()->void:
 	var tween := create_tween()
 	tween.tween_property(game_over_text, "position",screen_center.position, 1.0)
-	tween.parallel().tween_property(game_over_buttons,"position",buttons_end.position,1.0)
+	tween.parallel().tween_property(game_over_buttons,"position",buttons_end.global_position,1.0)
+	
 	var jump_up_position = Vector2(screen_center.position.x, screen_center.position.y -50) 
 	tween.tween_property(game_over_text, "position",jump_up_position, 0.5)
 	tween.tween_property(game_over_text, "position",screen_center.position, 0.5)
