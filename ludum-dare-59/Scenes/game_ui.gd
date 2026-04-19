f99f6c467f94b8331999d741f65455452b1d7589
Erik Rodriguez <Luffiez123@gmail.com>
2026-04-19 12:57:16 +0200

    game controls fixes

--- ludum-dare-59/Scenes/game_ui.gd
+++ ludum-dare-59/Scenes/game_ui.gd
@@ -2,16 +2,19 @@ extends Control
 class_name  GameUI
 @export var screen_center: RichTextLabel
 @export var game_over_text : RichTextLabel
+@export var objective_text : RichTextLabel
+@export var buttons_end : RichTextLabel
+@export var buttons_end2 : RichTextLabel
 @export var scoreButton : Button
+@export var inputSpeedButton : Button
+@export var inputFocusButton : Button
 @export var light_house : Lighthouse
 @export var game_over_buttons : VBoxContainer
-@export var buttons_end : RichTextLabel
 var score:=0
 
 func _ready() -> void:
 	scoreButton.text = str(score)
 	light_house.on_game_over.connect(on_game_over)
-
 	
 func gain_score(score_to_add:int)->void:
 	score += score_to_add
@@ -25,3 +28,9 @@ func on_game_over()->void:
 	var jump_up_position = Vector2(screen_center.position.x, screen_center.position.y -50) 
 	tween.tween_property(game_over_text, "position",jump_up_position, 0.5)
 	tween.tween_property(game_over_text, "position",screen_center.position, 0.5)
+	
+	scoreButton.global_position = buttons_end2.global_position
+	
+	inputSpeedButton.visible = false
+	inputFocusButton.visible = false
+	objective_text.visible = false
